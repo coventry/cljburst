7f48cbde17e8d8a8fb6a3b9801ca950e41e3ebe2
Alex Coventry <coventry@gmail.com>
2015-01-04 19:14:22 -0500

    lein new app cljburst

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cljburst
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cljburst/core_test.clj
@@ -0,0 +1,7 @@
+(ns cljburst.core-test
+  (:require [clojure.test :refer :all]
+            [cljburst.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
